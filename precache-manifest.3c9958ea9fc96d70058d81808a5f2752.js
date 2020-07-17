4842b6f779ebe53860bd6f3078110a841a30435a
Irina Fylyppova (via Travis CI) <irina.h.fylyppova@gmail.com>
2020-07-17 09:20:05 +0000

    Deploy garevna/commsware to github.com/garevna/commsware.git:gh-pages

---
+++ precache-manifest.3c9958ea9fc96d70058d81808a5f2752.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89834dd56165c8391bed",
+    "url": "/commsware/css/app.5c837e77.css"
+  },
+  {
+    "revision": "58252139ef2faf032a11",
+    "url": "/commsware/css/chunk-vendors.ae7a5099.css"
+  },
+  {
+    "revision": "e991dea9f65ce799dd72095ab7ad6e88",
+    "url": "/commsware/fonts/Oswald-Bold.e991dea9.ttf"
+  },
+  {
+    "revision": "4be970a1b24aa834afff4b16842c8981",
+    "url": "/commsware/fonts/Oswald-Light.4be970a1.ttf"
+  },
+  {
+    "revision": "e1996192b98a516646ff9a8c0c0ca90c",
+    "url": "/commsware/fonts/Oswald-Regular.e1996192.ttf"
+  },
+  {
+    "revision": "834291c96a86d573ec28f1df66f222de",
+    "url": "/commsware/fonts/oswald-bold.834291c9.eot"
+  },
+  {
+    "revision": "a4426c8cdf87b988ae911ccfde91f03f",
+    "url": "/commsware/fonts/oswald-bold.a4426c8c.woff"
+  },
+  {
+    "revision": "e9feff9e68a6991265d909578bdbcfb8",
+    "url": "/commsware/fonts/oswald-light.e9feff9e.woff"
+  },
+  {
+    "revision": "ff2c74fcd49aaffdb80eda8c9a7da59f",
+    "url": "/commsware/fonts/oswald-light.ff2c74fc.eot"
+  },
+  {
+    "revision": "53b305745daa0067ceb5e6135756c029",
+    "url": "/commsware/fonts/oswald-regular.53b30574.eot"
+  },
+  {
+    "revision": "9a0ad4b565fe64de2a5e9e60ebba78a2",
+    "url": "/commsware/fonts/oswald-regular.9a0ad4b5.woff"
+  },
+  {
+    "revision": "310595a850bc175896a8de876251695f",
+    "url": "/commsware/img/logo-with-name.310595a8.svg"
+  },
+  {
+    "revision": "f6fb9de088e8b1542ab9fc9b9d80373d",
+    "url": "/commsware/img/phone.f6fb9de0.svg"
+  },
+  {
+    "revision": "ffa840f8163c65df0658bcfda74471a4",
+    "url": "/commsware/index.html"
+  },
+  {
+    "revision": "89834dd56165c8391bed",
+    "url": "/commsware/js/app.24a53f1b.js"
+  },
+  {
+    "revision": "58252139ef2faf032a11",
+    "url": "/commsware/js/chunk-vendors.95d14fa0.js"
+  },
+  {
+    "revision": "6a0346cb8dcc1e7993030a2a66b18432",
+    "url": "/commsware/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/commsware/robots.txt"
+  }
+]);
